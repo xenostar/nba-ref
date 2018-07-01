c51d021da5c8c0c67f52bb01a57621edafef2eb4
xenostar <bryan.a.leewood@gmail.com>
2018-07-01 19:15:41 -0400

    Scaffolding

--- src/App.old.js
+++ src/App.old.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import logo from './logo.svg';
+// import './_styles.css';
 
 class App extends Component {
   render() {
