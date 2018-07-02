06db4fc6d1c030e1afb72f964eaaa1e81162a8ff
xenostar <bryan.a.leewood@gmail.com>
2018-07-02 11:23:07 -0400

    Updating table styles. I like this look so far.

--- src/pages/teams/Teams.jsx
+++ src/pages/teams/Teams.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './_teams.css'
 
 // Vars
-var pull_url = 'https://api.mysportsfeeds.com/v1.2/pull/nba/latest/roster_players.json?team=ind'
+var pull_url = 'https://api.mysportsfeeds.com/v1.2/pull/nba/latest/roster_players.json?team=ind&rosterstatus=assigned-to-roster'
 var username = 'xenostar'
 var password = 'testpass123'
 
@@ -31,7 +31,9 @@ export default class Teams extends Component {
 						<td>{player.player.JerseyNumber}</td>
 						<td>{player.player.FirstName} {player.player.LastName}</td>
 						<td>{player.player.Position}</td>
+						<td>{player.player.Age}</td>
 						<td>{player.player.Height}</td>
+						<td>{player.player.Weight} lbs</td>
 					</tr>
 				)
 			})
@@ -44,12 +46,17 @@ export default class Teams extends Component {
 	render() {
 		return (
 			<div className="page page-teams">
+				<div className="tb-name">Team Roster</div>
 				<table className="tb-roster">
 					<thead>
-						<th class="row-1 row-number">#</th>
-						<th class="row-2 row-name">Name</th>
-						<th class="row-3 row-position">Position</th>
-						<th class="row-4 row-height">Height</th>
+						<tr>
+							<th className="row-number">#</th>
+							<th className="row-name">Name</th>
+							<th className="row-position">Position</th>
+							<th className="row-age">Age</th>
+							<th className="row-height">Height</th>
+							<th className="row-weight">Weight</th>
+						</tr>
 					</thead>
 					<tbody>
 						{this.state.roster}
