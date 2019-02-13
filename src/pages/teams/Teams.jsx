3e2c672a2196f9dc23fdab79fcfc0e642c10ee8c
xenostar <bryan.a.leewood@gmail.com>
2019-02-13 02:33:30 -0500

    Adding season selector to team page

--- src/pages/teams/Teams.jsx
+++ src/pages/teams/Teams.jsx
@@ -17,13 +17,14 @@ export default class Teams extends Component {
 		this.state = {
 			roster : [],
 			team : 'ind',
+			season : '2018-2019',
 			isLoaded : false,
 		}
 	}
 
 	handleChange = (event) => {
 		this.setState({
-			team : event.target.value
+			[event.target.name] : event.target.value,
 		}, _ => {
 			this.handleFetch()
 		})
@@ -33,7 +34,7 @@ export default class Teams extends Component {
 		this.setState({
 			isLoaded : false,
 		}, _ => {
-			fetch(`https://api.mysportsfeeds.com/v1.2/pull/nba/2018-2019-regular/roster_players.json?team=${this.state.team}&rosterstatus=assigned-to-roster`, {
+			fetch(`https://api.mysportsfeeds.com/v1.2/pull/nba/${this.state.season}-regular/roster_players.json?team=${this.state.team}&rosterstatus=assigned-to-roster`, {
 				headers: {
 					'Authorization' : 'Basic ' + btoa(username + ':' + password),
 					'Cache-Control' : 'no-cache, no-store, must-revalidate'
@@ -60,9 +61,9 @@ export default class Teams extends Component {
 					isLoaded : true,
 				})
 			})
-			.catch(error => {
-				console.log('Request failed: ', error)
-			})
+			// .catch(error => {
+			// 	console.log('Request failed: ', error)
+			// })
 		})
 	}
 
@@ -90,39 +91,50 @@ export default class Teams extends Component {
 		return (
 			<div className="page page-teams">
 				<form>
-					<label>Select Team</label>
-					<select value={this.state.team} onChange={this.handleChange}>
-						<option value="atl">Atlanta Hawks</option>
-						<option value="bos">Boston Celtics</option>
-						<option value="bro">Brooklyn Nets</option>
-						<option value="chi">Chicago Bulls</option>
-						<option value="cle">Cleveland Cavaliers</option>
-						<option value="cha">Charlotte Hornets</option>
-						<option value="dal">Dallas Mavericks</option>
-						<option value="det">Detroit Pistons</option>
-						<option value="den">Denver Nuggets</option>
-						<option value="gsw">Golden State Warriors</option>
-						<option value="hou">Houston Rockets</option>
-						<option value="ind">Indiana Pacers</option>
-						<option value="lac">LA Clippers</option>
-						<option value="lal">Los Angeles Lakers</option>
-						<option value="mem">Memphis Grizzlies</option>
-						<option value="mia">Miami Heat</option>
-						<option value="mil">Milwaukee Bucks</option>
-						<option value="min">Minnesota Timberwolves</option>
-						<option value="nop">New Orleans Pelicans</option>
-						<option value="nyk">New York Knicks</option>
-						<option value="okl">Oklhahoma City</option>
-						<option value="orl">Orlando Magic</option>
-						<option value="phx">Pheonix Suns</option>
-						<option value="phi">Philadelphia 76ers</option>
-						<option value="por">Portland Trailblazers</option>
-						<option value="sac">Sacremento Kings</option>
-						<option value="sas">San Antonio Spurs</option>
-						<option value="tor">Toronto Raptors</option>
-						<option value="uta">Utah Jazz</option>
-						<option value="was">Washington Wizards</option>
-					</select>
+					<div>
+						<label>Select Team</label>
+						<select name="team" value={this.state.team} onChange={this.handleChange}>
+							<option value="atl">Atlanta Hawks</option>
+							<option value="bos">Boston Celtics</option>
+							<option value="bro">Brooklyn Nets</option>
+							<option value="chi">Chicago Bulls</option>
+							<option value="cle">Cleveland Cavaliers</option>
+							<option value="cha">Charlotte Hornets</option>
+							<option value="dal">Dallas Mavericks</option>
+							<option value="det">Detroit Pistons</option>
+							<option value="den">Denver Nuggets</option>
+							<option value="gsw">Golden State Warriors</option>
+							<option value="hou">Houston Rockets</option>
+							<option value="ind">Indiana Pacers</option>
+							<option value="lac">LA Clippers</option>
+							<option value="lal">Los Angeles Lakers</option>
+							<option value="mem">Memphis Grizzlies</option>
+							<option value="mia">Miami Heat</option>
+							<option value="mil">Milwaukee Bucks</option>
+							<option value="min">Minnesota Timberwolves</option>
+							<option value="nop">New Orleans Pelicans</option>
+							<option value="nyk">New York Knicks</option>
+							<option value="okl">Oklhahoma City</option>
+							<option value="orl">Orlando Magic</option>
+							<option value="phx">Pheonix Suns</option>
+							<option value="phi">Philadelphia 76ers</option>
+							<option value="por">Portland Trailblazers</option>
+							<option value="sac">Sacremento Kings</option>
+							<option value="sas">San Antonio Spurs</option>
+							<option value="tor">Toronto Raptors</option>
+							<option value="uta">Utah Jazz</option>
+							<option value="was">Washington Wizards</option>
+						</select>
+					</div>
+					<div>
+						<label>Select Seasons</label>
+						<select name="season" value={this.state.season} onChange={this.handleChange}>
+						<option value="2018-2019">2018-2019</option>
+						<option value="2017-2018">2017-2018</option>
+						<option value="2016-2017">2016-2017</option>
+						<option value="2015-2016">2015-2016</option>
+						</select>
+					</div>
 				</form>
 				<Table tableTitle="Team Roster" tableData={tableData} isLoaded={this.state.isLoaded}>{this.state.roster}</Table>
 			</div>
