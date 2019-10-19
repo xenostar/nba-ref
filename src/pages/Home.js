0d514df828e29bd8b35da8b8818bbda4fae0b05a
xenostar <bryan.a.leewood@gmail.com>
2019-10-18 21:36:09 -0400

    Adding react-vis

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react'
 import styled from 'styled-components'
-import teams from 'api/teams.js'
+import teams from 'api/teams'
 
 const StyledHome = styled.div`
   display: grid;
