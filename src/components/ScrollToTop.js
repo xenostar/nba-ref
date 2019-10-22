b935236a21fdd7c290ca98cb98e76d9a1b176249
xenostar <bryan.a.leewood@gmail.com>
2019-10-22 11:26:16 -0400

    Removing smooth behavior

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -6,8 +6,8 @@ export const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // window.scrollTo(0,0);
-    window.scrollTo({ behavior: 'smooth', top: 0 });
+    window.scrollTo(0,0);
+    // window.scrollTo({ behavior: 'smooth', top: 0 });
     // window.scrollTo({ behavior: 'smooth', top: myRef.current.offsetTop })
   }, [pathname]);
 
