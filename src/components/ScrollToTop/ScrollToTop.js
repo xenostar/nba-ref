8a67517c8254ef69d49422f2994b7ee2b64a2282
xenostar <bryan.a.leewood@gmail.com>
2019-10-26 17:48:17 -0400

    Removing spaces

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const {pathname} = useLocation();
 
   useEffect(() => {
     window.scrollTo(0,0);
