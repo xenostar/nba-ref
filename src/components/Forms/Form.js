ac80e7694e60a17dc9069b881231f0ffad355480
xenostar <bryan.a.leewood@gmail.com>
2019-10-30 13:45:39 -0400

    Getting rid of floats.

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -1,20 +1,22 @@
 import styled from 'styled-components'
 
 export const Form = styled.form`
+  display:flex;
   margin-bottom: 2.5rem;
   overflow: hidden;
+  @media only screen and (max-width: 62.5rem) {
+    flex-direction: column;
+  }
 
   div {
     display: flex;
-    float: left;
     margin-left: 20px;
   }
   div:first-child {
     margin-left: 0;
   }
-  @media only screen and (max-width: 1000px) {
+  @media only screen and (max-width: 62.5rem) {
     div {
-      float: none;
       margin-left: 0;
       margin-bottom: 5px;
     }
