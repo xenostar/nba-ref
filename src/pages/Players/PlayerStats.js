e5552bdfbfc2334c8a4c8c8a17a3d36cec7ce59d
xenostar <bryan.a.leewood@gmail.com>
2019-11-07 13:10:48 -0500

    Fixing ugly function

--- src/pages/Players/PlayerStats.js
+++ src/pages/Players/PlayerStats.js
@@ -34,8 +34,8 @@ export const PlayerStats = ({values}) => {
 
   const camelCaseToWords = str => {
     return str.match(/^[a-z]+|[A-Z][a-z]*/g).map(function(x) {
-        return x[0].toUpperCase() + x.substr(1).toLowerCase();
-    }).join(' ');
+      return x[0].toUpperCase() + x.substr(1).toLowerCase()
+    }).join(' ')
   }
 
   const handleFetch = useCallback(async () => {
