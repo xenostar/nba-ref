30554d85879b22d8df44cab5f1bf832655123c8a
xenostar <bryan.a.leewood@gmail.com>
2019-11-19 13:00:28 -0500

    Getting password modal ready

---
+++ src/components/Modals/PasswordModal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import styled, { keyframes } from 'styled-components'
+import { useEffect } from 'react'
+
+export const PasswordModal = () => {
+
+  useEffect(() => {
+    console.log('Hello world')
+  }, [])
+
+  return (
+    <div>Hello world</div>
+  )
+}
